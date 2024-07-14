71e87a9f61bd8f86efa447eeefdce7407d2a9471
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-14 14:43:42 +0530

    feat:- Activity 3 Data types done

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,9 +1,22 @@
 var number = 25;
-console.log(number)
+// console.log(number);
 
-let string = 'Ashish'
-console.log(string)
+let string = "Ashish";
+// console.log(string);
 
-const logedIn = 'true'
+const logedIn = "true";
 
-console.log(logedIn)
+// console.log(logedIn);
+
+const Schoolname = {
+  name: "HSS Noida",
+  addres: "neare greater Kailas",
+};
+
+const friendName = ["Ashish", "Suresh", "Rohan", "Aman"];
+
+console.log(typeof number) // number
+console.log(typeof string) // string
+console.log(typeof logedIn) // string
+console.log(typeof Schoolname) // object
+console.log(typeof friendName) // object
