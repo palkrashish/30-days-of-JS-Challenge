96072215827666c3d50eb77b0fdc81386139bfc8
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-25 17:10:42 +0530

    feat:- Activity 2 Task 6 done

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,9 +1,16 @@
-let num = 20;
-let num1 = 10;
+// let num = 20;
+// let num1 = 10;
 
 // console.log("Sum of two number:", num + num1)
 // console.log("Subtraction of two number:", num - num1)
 // console.log("Multiplication of two number:", num * num1)
 // console.log("Divide of two number:", num / num1)
-console.log("Remainder of two number:", num % num1)
+// console.log("Remainder of two number:", num % num1)
 
+// ----------------------------------------------- Activity 2 -----------------------------
+
+let val = 6
+let val1 = 2
+
+val += val1
+console.log('Addition of two number:', val) // 8
