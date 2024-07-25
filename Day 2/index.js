076c26fd7aed9049c0fe4c69e68c2deedc9ce66a
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-25 17:03:51 +0530

    feat:- Task 5 done

--- Day 2/index.js
+++ Day 2/index.js
@@ -4,5 +4,6 @@ let num1 = 10;
 // console.log("Sum of two number:", num + num1)
 // console.log("Subtraction of two number:", num - num1)
 // console.log("Multiplication of two number:", num * num1)
-console.log("Divide of two number:", num / num1)
+// console.log("Divide of two number:", num / num1)
+console.log("Remainder of two number:", num % num1)
 
