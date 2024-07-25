3ee5a6dff99bdcebc9d94455295e6f052afa5da4
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-25 17:01:50 +0530

    feat:- Task 2 done

--- Day 2/index.js
+++ Day 2/index.js
@@ -1,4 +1,6 @@
-let num = 10;
-let num1 = 20;
+let num = 20;
+let num1 = 10;
 
-console.log("Sum of two number:", num + num1)
+// console.log("Sum of two number:", num + num1)
+console.log("Subtraction of two number:", num - num1)
+
