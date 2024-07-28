fb4180309306d8f89b8256aaffbbfe2fc5751a88
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-29 00:08:11 +0530

    feat:- Task 1 done

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,9 +1,10 @@
-// Activity 1 
+// Task 1
 
-let number = 10 
-if(number >=0){
-    console.log('Number is positive')
-}
-else{
-    console.log('Number is Negative')
+let number = 10;
+if (number > 0) {
+  console.log(`Number ${number} is Positive`);
+} else if (number < 0) {
+  console.log(`Number ${number} is Negative`);
+} else {
+  console.log(`Number ${number} is Zero`);
 }
