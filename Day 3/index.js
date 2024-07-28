045ac61facf3f83cabcb8a5d5526257dcc925dfd
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-29 00:11:54 +0530

    feat: - Task2 done

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,10 +1,19 @@
-// Task 1
+// // Task 1
 
-let number = 10;
-if (number > 0) {
-  console.log(`Number ${number} is Positive`);
-} else if (number < 0) {
-  console.log(`Number ${number} is Negative`);
-} else {
-  console.log(`Number ${number} is Zero`);
-}
+// let number = 10;
+// if (number > 0) {
+//   console.log(`Number ${number} is Positive`);
+// } else if (number < 0) {
+//   console.log(`Number ${number} is Negative`);
+// } else {
+//   console.log(`Number ${number} is Zero`);
+// }
+
+// Task 2
+
+let age = 19
+ if (age >=18){
+    console.log(`You are ${age} old, you are eligble for vote`)
+ } else{
+    console.log(`You are below of 18 Years old, you are not eligible for vote`)
+ }
