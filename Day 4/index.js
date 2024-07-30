7175724ac025d493ac988d694766f718585a8220
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-31 01:09:39 +0530

    feat:- Task 2 done Table

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,8 +1,15 @@
 // Task 1
 
-let number = 10;
-for (let i = 1; i <= number; i++) {
-  console.log(i);
+// let number = 10;
+// for (let i = 1; i <= number; i++) {
+//   console.log(i);
+// }
+
+// Task 2
+
+console.log(` Multiplication of Table  `);
+let table = 5;
+for (let i = 1; i <= 10; i++) {
+  let result = table * i;
+  console.log(`${table} * ${i} = ${result}`);
 }
-
-
