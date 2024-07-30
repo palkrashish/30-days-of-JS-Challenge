e37ba1b96b47615b67424308f3562047c50f0716
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-31 00:00:48 +0530

    feat:- Task 3

--- Day 3/index.js
+++ Day 3/index.js
@@ -11,9 +11,25 @@
 
 // Task 2
 
-let age = 19
- if (age >=18){
-    console.log(`You are ${age} old, you are eligble for vote`)
- } else{
-    console.log(`You are below of 18 Years old, you are not eligible for vote`)
- }
+// let age = 19
+//  if (age >=18){
+//     console.log(`You are ${age} old, you are eligble for vote`)
+//  } else{
+//     console.log(`You are below of 18 Years old, you are not eligible for vote`)
+//  }
+let num = 5;
+let num1 = 10;
+let num2 = 15;
+if (num >= num1) {
+  if (num >= num2) {
+    console.log(`Number ${num} is greater than ${num1} and ${num2} `);
+  } else {
+    console.log(` Number ${num2} is greater than ${num} and ${num1} `);
+  }
+} else {
+  if (num1 >= num2) {
+    console.log(` Number  ${num1} is greater than  ${num} and ${num2} `);
+  } else {
+    console.log(`Number ${num2} is greter than ${num} and ${num1} `);
+  }
+}
