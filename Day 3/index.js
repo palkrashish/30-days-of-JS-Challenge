41980c928223389aa55047d59e0fa8892b153194
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-31 00:13:03 +0530

    feat:-  Task 4 done

--- Day 3/index.js
+++ Day 3/index.js
@@ -17,19 +17,61 @@
 //  } else{
 //     console.log(`You are below of 18 Years old, you are not eligible for vote`)
 //  }
-let num = 5;
-let num1 = 10;
-let num2 = 15;
-if (num >= num1) {
-  if (num >= num2) {
-    console.log(`Number ${num} is greater than ${num1} and ${num2} `);
-  } else {
-    console.log(` Number ${num2} is greater than ${num} and ${num1} `);
+
+// let num = 5;
+// let num1 = 10;
+// let num2 = 15;
+// if (num >= num1) {
+//   if (num >= num2) {
+//     console.log(`Number ${num} is greater than ${num1} and ${num2}. `);
+//   } else {
+//     console.log(` Number ${num2} is greater than ${num} and ${num1}. `);
+//   }
+// } else {
+//   if (num1 >= num2) {
+//     console.log(` Number  ${num1} is greater than  ${num} and ${num2}. `);
+//   } else {
+//     console.log(`Number ${num2} is greter than ${num} and ${num1}. `);
+//   }
+// }
+
+// Task 4
+
+function getDayName({ dayNumber: number }) {
+  let dayName = "";
+
+  switch (dayNumber) {
+    case 1:
+      dayName = "Sunday";
+      break;
+    case 2:
+      dayName = "Monday";
+      break;
+    case 3:
+      dayName = "Tuesday";
+      break;
+    case 4:
+      dayName = "Wednesday";
+      break;
+    case 5:
+      dayName = "Thursday";
+      break;
+    case 6:
+      dayName = "Friday";
+      break;
+    case 7:
+      dayName = "Saturday";
+      break;
+    default:
+      dayName = "Invalid day number. Please enter a number between 1 and 7.";
+      break;
   }
-} else {
-  if (num1 >= num2) {
-    console.log(` Number  ${num1} is greater than  ${num} and ${num2} `);
-  } else {
-    console.log(`Number ${num2} is greter than ${num} and ${num1} `);
-  }
+
+  return dayName;
 }
+
+let dayNumber = 5;
+
+let dayName = getDayName({ dayNumber });
+
+console.log(`Day ${dayNumber} is ${dayName}.`);
