82c277ae90cdda389392522ec2cd3259ed8111f7
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-01 00:34:28 +0530

    feat: - Task 3 done

--- Day 4/index.js
+++ Day 4/index.js
@@ -7,9 +7,20 @@
 
 // Task 2
 
-console.log(` Multiplication of Table  `);
-let table = 5;
-for (let i = 1; i <= 10; i++) {
-  let result = table * i;
-  console.log(`${table} * ${i} = ${result}`);
+// console.log(` Multiplication of Table  `);
+// let table = 5;
+// for (let i = 1; i <= 10; i++) {
+//   let result = table * i;
+//   console.log(`${table} * ${i} = ${result}`);
+// }
+
+// Task 3
+let num = 1;
+let sum = 0;
+
+while (num <= 10) {
+    sum = sum + num;
+    num = num + 1;
 }
+
+console.log("The sum of numbers from 1 to 10 is:", sum);
