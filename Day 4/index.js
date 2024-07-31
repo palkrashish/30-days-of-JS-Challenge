ac03b1fa0483ae8b168601b16a22aa45f42c2bba
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-01 00:49:42 +0530

    feat:- Task 9 done

--- Day 4/index.js
+++ Day 4/index.js
@@ -74,11 +74,20 @@
 // }
 
 
-// Task 8 
+// // Task 8 
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 5) {
+//       continue; 
+//   }
+//   console.log(i);
+// }
+
+// Task 9
 
 for (let i = 1; i <= 10; i++) {
-  if (i === 5) {
-      continue; 
+  if (i === 7) {
+      break; 
   }
   console.log(i);
 }
