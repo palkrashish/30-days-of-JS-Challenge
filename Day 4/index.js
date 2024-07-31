e26e1fd0970abea84b861924520adc7865405986
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-01 00:38:51 +0530

    feat:- Task 4 done

--- Day 4/index.js
+++ Day 4/index.js
@@ -15,12 +15,21 @@
 // }
 
 // Task 3
-let num = 1;
-let sum = 0;
+// let num = 1;
+// let sum = 0;
 
-while (num <= 10) {
-    sum = sum + num;
-    num = num + 1;
+// while (num <= 10) {
+//     sum = sum + num;
+//     num = num + 1;
+// }
+
+// console.log("The sum of numbers from 1 to 10 is:", sum);
+
+// Task 4
+
+console.log("Print the number from 10 to 1: -");
+let num = 10;
+while (num >= 1) {
+  console.log(` ${num}`);
+  num = num - 1;
 }
-
-console.log("The sum of numbers from 1 to 10 is:", sum);
