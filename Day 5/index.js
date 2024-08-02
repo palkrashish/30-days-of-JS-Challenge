82af6194639f9679931f2a92b407abd9457555ed
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-02 23:46:42 +0530

    feat: - Task 1 Done Even Odd

---
+++ Day 5/index.js
@@ -0,0 +1,10 @@
+function numberCheck(number){
+    if(number % 2 ==0){
+        console.log(`Number is Even: - ${number}`)
+    }else{
+        console.log(`Number is Odd: - ${number}`)
+    }
+}
+
+
+numberCheck(5)
