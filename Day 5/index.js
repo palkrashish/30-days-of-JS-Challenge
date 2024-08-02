2c8689142e5c7c933159a77cdf80f16a0106ce27
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-02 23:50:41 +0530

    feat: - Task 2 done square

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,10 +1,18 @@
-function numberCheck(number){
-    if(number % 2 ==0){
-        console.log(`Number is Even: - ${number}`)
-    }else{
-        console.log(`Number is Odd: - ${number}`)
-    }
+// Task 1
+// function numberCheck(number){
+//     if(number % 2 ==0){
+//         console.log(`Number is Even: - ${number}`)
+//     }else{
+//         console.log(`Number is Odd: - ${number}`)
+//     }
+// }
+
+// numberCheck(5)
+
+// Task 2
+
+function squareNumber(number) {
+  return number * number;
 }
-
-
-numberCheck(5)
+let result = squareNumber(5);
+console.log(`Square  of  number is: ${result} `);
