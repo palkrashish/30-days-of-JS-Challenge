c9b20f231e66d28373991d18fe66e502e500f8af
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-04 00:11:31 +0530

    feat: - Task 2 Access first element and last element

--- Day 6/index.js
+++ Day 6/index.js
@@ -1,5 +1,12 @@
-// Task 1
+ // Task 1
+// const array = [1, 2, 3, 4, 5];
+// console.log(array)
+
+// Task 2
 const array = [1, 2, 3, 4, 5];
-console.log(array)
 
-
+console.log(
+  `Access the first elements is '${array[0]}' and last element is '${
+    array.length - 1
+  }' `
+);
