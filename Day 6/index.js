ff7b34a99ed8d876861137e146542157b5b042d6
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-04 00:17:50 +0530

    feat:- Task 10 using for loop

--- Day 6/index.js
+++ Day 6/index.js
@@ -44,6 +44,13 @@
 // console.log(result);
 
 // Task 9
+// const array = [1, 2, 3, 4, 5];
+// const sumAllNumber = array.reduce((acc, curr) => acc + curr);
+// console.log("All array sum are: - ", sumAllNumber);
+
+// Task 10
 const array = [1, 2, 3, 4, 5];
-const sumAllNumber = array.reduce((acc, curr) => acc + curr);
-console.log("All array sum are: - ", sumAllNumber);
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(`Element of index '${index}' and number is '${element}'`);
+}
