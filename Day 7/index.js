616265c18c15ad76af9c1d7be00a2097b55ab2f2
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-05 00:15:33 +0530

    feat:- Task 6 all book prints

--- Day 7/index.js
+++ Day 7/index.js
@@ -48,10 +48,32 @@
 // Task 5
 
 // Define book object constructor
+// function Book(title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// }
+
+// // Create book objects
+// let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+// let book2 = new Book("1984", "George Orwell", 1949);
+// let book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
+
+// // Define the library object
+// let library = {
+//   name: "Example Library",
+//   books: [book1, book2, book3],
+// };
+
+// // Log the library object to the console
+// console.log(library);
+
+// Task 6
+// Define book object constructor
 function Book(title, author, year) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
+    this.title = title;
+    this.author = author;
+    this.year = year;
 }
 
 // Create book objects
@@ -61,9 +83,15 @@ let book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
 
 // Define the library object
 let library = {
-  name: "Example Library",
-  books: [book1, book2, book3],
+    name: "Example Library",
+    books: [book1, book2, book3]
 };
 
-// Log the library object to the console
-console.log(library);
+// Access and log the name of the library
+console.log("Library Name:", library.name);
+
+// Access and log the titles of all books in the library
+console.log("Books in the Library:");
+library.books.forEach(book => {
+    console.log(book.title);
+});
