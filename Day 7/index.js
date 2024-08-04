4ee39c8d9a5ae9b6c19b4cb17700b1bdd5af325b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-05 00:10:57 +0530

    feat: - Task 2 specific object log

--- Day 7/index.js
+++ Day 7/index.js
@@ -1,9 +1,18 @@
 // Task 1
+// const bookName = {
+//   title: "You do not know JS",
+//   author: "Kyle",
+//   year: 2024,
+// };
+
+// console.log(bookName)
+
+// Task 2
 const bookName = {
-  title: "You do not know JS",
-  author: "Kyle",
-  year: 2024,
-};
-
-console.log(bookName)
-
+    title: "You do not know JS",
+    author: "Kyle",
+    year: 2024,
+  };
+console.log(
+  `Best Book of JS is '${bookName.title}' and author is '${bookName.author}' relase year '${bookName.year}' `
+);
