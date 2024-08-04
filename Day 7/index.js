29449826459ce8ea6bc45e78711e1f2bf79ad9b8
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-05 00:14:10 +0530

    feat:- Task 5 created nested object

--- Day 7/index.js
+++ Day 7/index.js
@@ -30,17 +30,40 @@
 // console.log(bookName.bookInformation());
 
 // Task 4
-const bookName = {
-    title: "You do not know JS",
-    author: "Kyle",
-    year: 2024,
-    bookInformation: function () {
-      return this.title + " Author " + this.author;
-    },
-    updateYear: function(updateYear){
-        this.year = updateYear
-    }
-  };
-  console.log(bookName);
-  bookName.updateYear(2025)
-  console.log(bookName)
+// const bookName = {
+//     title: "You do not know JS",
+//     author: "Kyle",
+//     year: 2024,
+//     bookInformation: function () {
+//       return this.title + " Author " + this.author;
+//     },
+//     updateYear: function(updateYear){
+//         this.year = updateYear
+//     }
+//   };
+//   console.log(bookName);
+//   bookName.updateYear(2025)
+//   console.log(bookName)
+
+// Task 5
+
+// Define book object constructor
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
+
+// Create book objects
+let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+let book2 = new Book("1984", "George Orwell", 1949);
+let book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
+
+// Define the library object
+let library = {
+  name: "Example Library",
+  books: [book1, book2, book3],
+};
+
+// Log the library object to the console
+console.log(library);
