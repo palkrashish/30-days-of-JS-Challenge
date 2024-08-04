59ebec96f96609f15597e523218818bb2174a1b3
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-05 00:11:52 +0530

    feat:- Task 3 using this method

--- Day 7/index.js
+++ Day 7/index.js
@@ -8,11 +8,23 @@
 // console.log(bookName)
 
 // Task 2
+// const bookName = {
+//     title: "You do not know JS",
+//     author: "Kyle",
+//     year: 2024,
+//   };
+// console.log(
+//   `Best Book of JS is '${bookName.title}' and author is '${bookName.author}' relase year '${bookName.year}' `
+// );
+
+// Task 3
+
 const bookName = {
-    title: "You do not know JS",
-    author: "Kyle",
-    year: 2024,
-  };
-console.log(
-  `Best Book of JS is '${bookName.title}' and author is '${bookName.author}' relase year '${bookName.year}' `
-);
+  title: "You do not know JS",
+  author: "Kyle",
+  year: 2024,
+  bookInformation: function () {
+    return this.title + " Author " + this.author;
+  },
+};
+console.log(bookName.bookInformation());
