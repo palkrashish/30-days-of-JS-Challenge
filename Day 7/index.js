d0a5cf130d3741399e6b8b7d6038cd0bb9228ab7
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-05 00:16:34 +0530

    feat:- Task 7 this keyword return and print the log

--- Day 7/index.js
+++ Day 7/index.js
@@ -70,28 +70,49 @@
 
 // Task 6
 // Define book object constructor
+// function Book(title, author, year) {
+//     this.title = title;
+//     this.author = author;
+//     this.year = year;
+// }
+
+// // Create book objects
+// let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+// let book2 = new Book("1984", "George Orwell", 1949);
+// let book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
+
+// // Define the library object
+// let library = {
+//     name: "Example Library",
+//     books: [book1, book2, book3]
+// };
+
+// // Access and log the name of the library
+// console.log("Library Name:", library.name);
+
+// // Access and log the titles of all books in the library
+// console.log("Books in the Library:");
+// library.books.forEach(book => {
+//     console.log(book.title);
+// });
+
+
+// Task 7
+// Define book object constructor with a method
 function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
+  this.title = title;
+  this.author = author;
+  this.year = year;
+
+  // Method to get book information
+  this.getBookInfo = function () {
+    return this.title + " (" + this.year + ")";
+  };
 }
 
-// Create book objects
-let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
-let book2 = new Book("1984", "George Orwell", 1949);
-let book3 = new Book("The Catcher in the Rye", "J.D. Salinger", 1951);
+// Create a book object
+let book = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
 
-// Define the library object
-let library = {
-    name: "Example Library",
-    books: [book1, book2, book3]
-};
+// Log the result of calling the getBookInfo method
+console.log(book.getBookInfo());
 
-// Access and log the name of the library
-console.log("Library Name:", library.name);
-
-// Access and log the titles of all books in the library
-console.log("Books in the Library:");
-library.books.forEach(book => {
-    console.log(book.title);
-});
