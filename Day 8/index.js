50b4eef6a1c55e8e87696db61e3420312b7af4f1
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-07 16:00:41 +0530

    feat:- Task 2 multiline template

--- Day 8/index.js
+++ Day 8/index.js
@@ -1,6 +1,13 @@
 // Task 1
-let name = "Ashish";
-let age = 25;
+// let name = "Ashish";
+// let age = 25;
+// let fullName = `My name is  "${name}" and age is: ${age}`;
+// console.log(fullName)
 
-let fullName = `My name is  "${name}" and age is: ${age}`;
-console.log(fullName)
+
+// Task 2
+ let fullName = `My name is Ashish Kr Pal
+ Currently I live in Delhi. I love to drink chai.
+ `
+
+ console.log(fullName)
