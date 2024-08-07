6c5f371f10e0c182ee9cd5f7a29d2ed1522ea04b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-08-07 16:04:06 +0530

    feat:- Task 3 array destructuring

--- Day 8/index.js
+++ Day 8/index.js
@@ -4,10 +4,18 @@
 // let fullName = `My name is  "${name}" and age is: ${age}`;
 // console.log(fullName)
 
-
 // Task 2
- let fullName = `My name is Ashish Kr Pal
- Currently I live in Delhi. I love to drink chai.
- `
+//  let fullName = `My name is Ashish Kr Pal
+//  Currently I live in Delhi. I love to drink chai.
+//  `
 
- console.log(fullName)
+//  console.log(fullName)
+
+// Task 3
+
+let array = [1, 2, 3, 4];
+
+let [first, second] = array;
+
+console.log("First number", first);
+console.log("Second number", second);
